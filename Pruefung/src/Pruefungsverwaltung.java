a49bd8954ccf1545f9ebac01fdd838f247cd0d5e
DeeVeX <DeeVeX@160.85.34.211>
2013-10-24 18:30:47 +0200

    Wechsel von Laptop auf PC

--- Pruefung/src/Pruefungsverwaltung.java
+++ Pruefung/src/Pruefungsverwaltung.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * Bietet Funktionalitaeten zum Speichern von Pruefungsergebnissen von einer
  * Vielzahl von Studenten. Aus den gespeicherten Ergebnissen lassen sich
@@ -10,8 +12,27 @@ public class Pruefungsverwaltung {
    * 
    * @param ergebnis Das Pruefungsergebnis.
    */
+	
+	HashMap<String, String> studentenErgebnis = new HashMap<String, String>();
+	HashMap<Double, String> dictionaryDoubleText = new HashMap<Double, String>();
+	
+	public Pruefungsverwaltung () {
+		//Dictionary aufsetzen
+		dictionaryDoubleText.put(1.0, "eins");
+		dictionaryDoubleText.put(1.5, "eins punkt fuenf");
+		dictionaryDoubleText.put(2.0, "zwei");
+		dictionaryDoubleText.put(2.5, "zwei punkt fuenf");
+		dictionaryDoubleText.put(3.0, "drei");
+		dictionaryDoubleText.put(3.5, "drei punkt fuenf");
+		dictionaryDoubleText.put(4.0, "vier");
+		dictionaryDoubleText.put(4.5, "vier punkt fuenf");
+		dictionaryDoubleText.put(5.0, "fuenf");
+		dictionaryDoubleText.put(5.5, "fuenf punkt fuenf");
+		dictionaryDoubleText.put(6.0, "sechs");
+	}
+	
   public void speichereErgebnis(Pruefungsergebnis ergebnis) {
-    // TODO Implementieren gemaess Aufgabenstellung
+	  pruefungsergebnisToHashMap(ergebnis);
   }
 
   /**
@@ -25,11 +46,35 @@ public class Pruefungsverwaltung {
    * Herzliche Gratulation Max Muster! Sie haben an der Pruefung eine 4.5
    * (vier pounkt fuenf) erzielt und somit bestanden!
    */
+  	
   public void druckeAntworttexte() {
-    // TODO Implementieren gemaess Aufgabenstellung
+	  
   }
 
   private double rundeAufHalbeNote(double note) {
     return Math.round(note * 2) / 2.0;
+  }
+  
+  private void pruefungsergebnisToHashMap(Pruefungsergebnis ergebnis) {
+
+	  String antwortText = "";
+	  
+	  if (ergebnis.getNote() < 4.0)
+	  {
+		 antwortText = "Sie haben an der Pruefung eine " +
+				 		rundeAufHalbeNote(ergebnis.getNote()) + " (" +
+				 		dictionaryDoubleText.get(rundeAufHalbeNote(ergebnis.getNote())) + ")" +
+				 		"erzielt und sind somit durchgefallen!";
+	  }
+	  else if(ergebnis.getNote() >= 4.0)
+	  {
+			 antwortText = "Herzliche Gratulation " + ergebnis.getStudent() +
+					 	"! Sie haben an der Pruefung eine " +
+				 		rundeAufHalbeNote(ergebnis.getNote()) + " (" +
+				 		dictionaryDoubleText.get(rundeAufHalbeNote(ergebnis.getNote())) + ")" +
+				 		" erzielt und somit bestanden!";
+	  }
+	  
+	  studentenErgebnis.put(ergebnis.getStudent(), antwortText);
   }
 }
