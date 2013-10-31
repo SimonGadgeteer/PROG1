42327c4f3bda5ea22708098d15db0e6522f93e00
SimonGadgeteer <simon.schwarz@mail.com>
2013-10-31 21:20:47 +0100

    Wortstat II

--- Wortstatistik/src/Worthaeufigkeitsanalyse.java
+++ Wortstatistik/src/Worthaeufigkeitsanalyse.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Zaehlt die Anzahl Vorkommnisse von Woertern ueber mehrere Zeichenketten.
  * Es lassen sich eine beliebige Anzahl an Zeichenketten uebergeben. Die
@@ -8,10 +12,65 @@
  * Wort werten zu koennen.
  * 
  * @version 1.0
- * @author XXXX
+ * @author Dave Kramer, Simon Schwarz
  */
 public class Worthaeufigkeitsanalyse {
 
-
+	static private final HashSet<String> satzzeichen = new HashSet<String>();
+	private HashMap<String, Integer> wortSpeicher = new HashMap<String, Integer>();
 	
+	static
+	{
+		satzzeichen.add("?");
+		satzzeichen.add("!");
+		satzzeichen.add(".");
+		satzzeichen.add(",");
+		satzzeichen.add(":");
+		satzzeichen.add(";");
+	}
+	
+
+	public static void main(String[] args) {
+		Worthaeufigkeitsanalyse wha =new Worthaeufigkeitsanalyse();
+		wha.verarbeiteText("FRITZ ist!!!!!! doof, im fall");
+		wha.verarbeiteText(" !!!!!! doof, im fall");
+		wha.verarbeiteText(" !!!!!! doof, im fall");
+		wha.verarbeiteText(" ist!!!!!! doof, im ");
+		wha.druckeStatistik();
+	}
+
+
+	public void verarbeiteText(String inputText)
+	{
+		for(String satzzeichenitem : satzzeichen)
+		{
+			inputText = inputText.replace(satzzeichenitem, "");
+		}
+		inputText = inputText.toLowerCase();
+		
+		for(String wort : inputText.split(" "))
+		{
+			if(!wortSpeicher.keySet().contains(wort))
+			{
+				wortSpeicher.put(wort, 1);
+				System.out.println("bam");
+			}
+			else
+			{
+				int count = wortSpeicher.get(wort);
+				wortSpeicher.put(wort, ++count);
+			}
+		}
+		
+		System.out.println("sadf: "+inputText);
+	}
+	
+	public void druckeStatistik()
+	{	
+		
+		for(int i =0; i < wortSpeicher.size(); i++)
+		{
+			System.out.println(wortSpeicher.entrySet());
+		}
+	}
 }
