f9fc6625df71151a8a9bd6a4ea051af7b67b8d1b
SimonGadgeteer <simon.schwarz@mail.com>
2013-10-31 10:13:24 +0100

    Wortstatistik erstellt

---
+++ Wortstatistik/src/Worthaeufigkeitsanalyse.java
@@ -0,0 +1,17 @@
+/**
+ * Zaehlt die Anzahl Vorkommnisse von Woertern ueber mehrere Zeichenketten.
+ * Es lassen sich eine beliebige Anzahl an Zeichenketten uebergeben. Die
+ * Statistik wird fortlaufend gefuehrt. Die Wortzaehlung laesst sich jederzeit
+ * ausgeben. Die Satzeichen . , ? ! " : ; werden entfernt. Alle Buchstaben
+ * werden in Kleinbuchstaben umgewandelt um beispielsweise das Wort 'die'
+ * inmitten eines Satzes und das Wort 'Die' am Anfang eines Satzes als gleiches
+ * Wort werten zu koennen.
+ * 
+ * @version 1.0
+ * @author XXXX
+ */
+public class Worthaeufigkeitsanalyse {
+
+
+	
+}
