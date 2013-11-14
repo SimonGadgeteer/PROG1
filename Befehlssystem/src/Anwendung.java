8ecef44e8e769370d6fde1c9d3344ac255a5960c
SimonGadgeteer <simon.schwarz@mail.com>
2013-11-14 11:11:55 +0100

    created class "Anwendung"

--- Befehlssystem/src/Anwendung.java
+++ Befehlssystem/src/Anwendung.java
@@ -1,3 +1,39 @@
-public class Anwendung {
+/**
+ * sadf
+ *
+ * @author  Dave Kramer, Simon Schwarz
+ * @version 1.0
+ */
 
+public class Anwendung {
+	
+	private static Parser parser;
+	private static Kontroller kontroller;
+
+	public Anwendung()
+	{
+		
+	}
+	
+    public static void main(String args[])
+    {
+    	Anwendung anwendung = new Anwendung();
+    	anwendung.start();
+    }
+    
+    
+    public void start()
+    {
+    	parser = new Parser();
+    	kontroller = new Kontroller();
+    	boolean weiter = true;
+    	
+    	while(weiter)
+    	{
+    		Befehl input = parser.liefereBefehl();
+    		weiter = kontroller.verarbeiteBefehl(input);
+    	}
+    }
+    
+    
 }
