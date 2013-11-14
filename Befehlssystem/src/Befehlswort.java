32075698306d93d9f83d48d11e770e33789b3531
DeeVeX <DeeVeX@160.85.34.67>
2013-11-14 11:20:37 +0100

    Enumeriert

--- Befehlssystem/src/Befehlswort.java
+++ Befehlssystem/src/Befehlswort.java
@@ -8,46 +8,23 @@ import java.util.HashSet;
  * @author  tebe
  * @version 1.0
  */
-public class Befehlswort {
+public enum Befehlswort {
 
-	// Ein HashSet mit den gueltigen Befehlswoertern als Klassenvariable
-	private static final HashSet<String> gueltigeBefehle = new HashSet<String>();
-	static {
-		gueltigeBefehle.add("gehe");
-		gueltigeBefehle.add("beenden");
-		gueltigeBefehle.add("hilfe");
+	UNBEKANNT("unbekannt"), GEHE("gehe"), HILFE("hilfe"), BEENDEN("beenden");
+
+	private final String Befehl;
+	
+	Befehlswort(String Befehl){
+		this.Befehl = Befehl;
 	}
-
-	/**
-	 * Konstruktor - initialisiere die Befehlswörter.
-	 */
-	public Befehlswort()
-	{
-		// tut momentan nichts
-	}
-
-	/**
-	 * Pruefe, ob eine gegebene Zeichenkette ein gueltiger
-	 * Befehl ist.
-	 * 
-	 * @return 'true', wenn die gegebene Zeichenkette ein gueltiger
-	 *         Befehl ist, 'false' sonst.
-	 */
-	public static boolean istBefehl(String eingabe)
-	{
-		return gueltigeBefehle.contains(eingabe);
-	}
-
-	/**
-	* Gibt die akzeptierten Befehlsworte als Text zurueck.
-	*
-	* @return Die akzeptierten Befehlsworte als Text
-	*/
-	public static String gibBefehlsworteAlsText() {
-	              String text = "";
-	              for (String befehl : gueltigeBefehle) {
-	                             text = text + befehl + " ";
-	              }
-	              return text;
+	
+	public static Befehlswort gibBefehlsWort(String wort) {
+		for (Befehlswort befehlswort : Befehlswort.values()) {
+			if (wort.equals(befehlswort.toString()))
+			{
+				return befehlswort;
+			}
+		}
+		return Befehlswort.UNBEKANNT;
 	}
 }
