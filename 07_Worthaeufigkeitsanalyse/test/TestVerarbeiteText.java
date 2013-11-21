a7869541fd956f57bee03e1622541254e2e0b964
SimonGadgeteer <simon.schwarz@mail.com>
2013-11-21 11:27:32 +0100

    1.b) in progress

--- 07_Worthaeufigkeitsanalyse/test/TestVerarbeiteText.java
+++ 07_Worthaeufigkeitsanalyse/test/TestVerarbeiteText.java
@@ -4,10 +4,33 @@ import org.junit.Test;
 
 
 public class TestVerarbeiteText {
+	private Worthaeufigkeitsanalyse wortAnalyse;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void korrekterText() {
+		wortAnalyse = new Worthaeufigkeitsanalyse();
+		String wort = "Dies ist ein Testsatz, mit ganz vielen Satzzeichen! Wirklich?";
+		String erwartung="";
+		
+		assertEquals(erwartung, wortAnalyse.entferneSatzzeichen(wort));
+	}
+	
+	@Test
+	public void wiederholenderText() {
+		wortAnalyse = new Worthaeufigkeitsanalyse();
+		String wort = "Dies ist ein Testsatz, mit ganz vielen Satzzeichen! Wirklich? Wirklich ist ein Wort";
+		String erwartung="";
+		
+		assertEquals(erwartung, wortAnalyse.entferneSatzzeichen(wort));
+	}
+	
+	@Test
+	public void falscherText() {
+		wortAnalyse = new Worthaeufigkeitsanalyse();
+		String wort = "2390 89032 :;: 32434";
+		String erwartung="";
+		
+		assertEquals(erwartung, wortAnalyse.entferneSatzzeichen(wort));
 	}
 
 }
